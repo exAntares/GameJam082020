07397efcef6cb6c0222feb321defb1420585a9ec
Luis Rojas <luis_andres81@hotmail.com>
2020-08-08 01:20:21 +0200

    New camera controller

---
+++ Assets/GameJam/Scripts/CameraController.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+    [SerializeField] private float _speed = 1.0f;
+    [SerializeField] private Camera _camera;
+    
+    private Vector3? _lastKnownPos;
+
+    void FixedUpdate() {
+        if (Input.GetMouseButton(0)) {
+            if (_lastKnownPos != null) {
+                var moveDiffX = Input.mousePosition.x - _lastKnownPos.Value.x;
+                var moveDiffY = Input.mousePosition.y - _lastKnownPos.Value.y;
+                var speed = _speed * Time.fixedDeltaTime;
+                var addPos = new Vector3(moveDiffX * speed, 0, moveDiffY * speed);
+                transform.position += addPos;
+            }
+        }
+            
+        _lastKnownPos = Input.mousePosition;
+    }
+}
