0d2d1f56e4624546d52a8893aecf42bfecd47731
Luis Rojas <luis_andres81@hotmail.com>
2020-08-08 18:17:05 +0200

    Improved lighting

--- Assets/GameJam/Scripts/ScenesLoader.cs
+++ Assets/GameJam/Scripts/ScenesLoader.cs
@@ -5,10 +5,5 @@ public class ScenesLoader : MonoBehaviour
 {
     void Awake() {
         SceneManager.LoadScene(sceneBuildIndex: 1, LoadSceneMode.Additive);
-        SceneManager.LoadScene(sceneBuildIndex: 2, LoadSceneMode.Additive);
-    }
-
-    private void Start() {
-        SceneManager.SetActiveScene(SceneManager.GetSceneAt(1));
     }
 }
