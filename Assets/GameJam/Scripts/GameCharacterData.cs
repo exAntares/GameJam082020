4f2b7ff706a58345f028be0c41620e2589a2bd81
Luis Rojas <luis_andres81@hotmail.com>
2020-08-08 17:54:29 +0200

    Added scenes loader to merge scenes

---
+++ Assets/GameJam/Scripts/GameCharacterData.cs
@@ -0,0 +1,9 @@
+﻿using HalfBlind.ScriptableVariables;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = nameof(GameCharacterData), menuName = "ScriptableObjects/GameJam/"+nameof(GameCharacterData))]
+public class GameCharacterData : ScriptableObject {
+    public Sprite Icon;
+    public ScriptableGameEvent OnSuccess;
+    public ScriptableGameEvent OnFail;
+}
---
+++ Assets/GameJam/Scripts/ScenesLoader.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ScenesLoader : MonoBehaviour
+{
+    void Awake() {
+        SceneManager.LoadScene(sceneBuildIndex: 1, LoadSceneMode.Additive);
+        SceneManager.LoadScene(sceneBuildIndex: 2, LoadSceneMode.Additive);
+    }
+
+    private void Start() {
+        SceneManager.SetActiveScene(SceneManager.GetSceneAt(1));
+    }
+}
