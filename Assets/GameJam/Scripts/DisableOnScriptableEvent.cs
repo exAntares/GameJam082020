302b74fd9201d0cc3881a18fb1891a0a94c33856
Luis Rojas <luis_andres81@hotmail.com>
2020-08-09 21:21:26 +0200

    Disable hud on game win

---
+++ Assets/GameJam/Scripts/DisableOnScriptableEvent.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+namespace HalfBlind.ScriptableVariables {
+    public class DisableOnScriptableEvent : MonoBehaviour {
+        [SerializeField] private ScriptableGameEvent _event;
+
+        private void Awake() {
+            _event.AddListener(OnEvent);
+        }
+
+        private void OnDestroy() {
+            _event.RemoveListener(OnEvent);
+        }
+
+        private void OnEvent() {
+            gameObject.SetActive(false);
+        }
+    }
+}
