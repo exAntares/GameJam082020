09f9265b49a4f15abdd1e0bb4fc8d9f42fcf9756
Luis Rojas <luis_andres81@hotmail.com>
2020-08-09 21:16:14 +0200

    small tweaks on hud

--- Assets/GameJam/Scripts/CharacterSetter.cs
+++ Assets/GameJam/Scripts/CharacterSetter.cs
@@ -1,14 +1,28 @@
-﻿using UnityEngine;
+﻿using DG.Tweening;
+using UnityEngine;
 using UnityEngine.UI;
 
 public class CharacterSetter : MonoBehaviour {
     public GameController Controller;
     public Image[] _images;
+    public bool ShouldDisableOnFound;
+    public Ease OutEase;
 
     void Start() {
         var selectedCharacters = Controller.GetCharacters();
         for (var i = 0; i < selectedCharacters.Length; i++) {
-            _images[i].sprite = selectedCharacters[i].Icon;
+            var gameCharacterData = selectedCharacters[i];
+            var image = _images[i];
+            void LocalFunc() {
+                OutEase = Ease.OutSine;
+                image.DOFade(0.0f, 1.0f)
+                    .SetEase(OutEase);
+                gameCharacterData.OnSuccess.RemoveListener(LocalFunc);
+            }
+            image.sprite = gameCharacterData.Icon;
+            if (ShouldDisableOnFound) {
+                gameCharacterData.OnSuccess.AddListener(LocalFunc);
+            }
         }
     }
 }
